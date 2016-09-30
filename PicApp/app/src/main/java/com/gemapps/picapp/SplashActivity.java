c86f9a48c5c25bad95b55bf3571c190c8db6c75c
edug <edu@funler.com>
2016-09-30 16:08:45 -0300

    Add logo and give logic to the splash screen

--- PicApp/app/src/main/java/com/gemapps/picapp/SplashActivity.java
+++ PicApp/app/src/main/java/com/gemapps/picapp/SplashActivity.java
@@ -1,12 +1,20 @@
 package com.gemapps.picapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gemapps.picapp.ui.PhotoListActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, PhotoListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
